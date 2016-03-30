0d79bb232f41b8966319b45610a1de5f9ef020c5
Kevron Rees <tripero.kev@gmail.com>
2016-03-30 07:37:28 -0700

    trying to get it to install __init__.py

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,28 +4,41 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 import os
+import sys
 
 src_root = "./"
 install_prefix= "usr/"
-lib_prefix = "{}lib".format(install_prefix)
+lib_dir = os.path.join(install_prefix, 'lib')
+include_dir = os.path.join(src_root, install_prefix, "include", "soletta")
 
 if "SOLETTA_SRC_ROOT" in os.environ:
 	src_root = os.environ["SOLETTA_SRC_ROOT"]
 
-if "INSTALL_ROOT" in os.environ:
-	install_prefix = os.environ["INSTALL_ROOT"]
+if "PREFIX" in os.environ:
+	install_prefix = os.environ["PREFIX"]
 
-if "SOLETTA_LIBRARY_PATH" in os.environ:
-	lib_prefix = os.environ["SOLETTA_LIBRARY_PATH"]
+if "LIBDIR" in os.environ:
+	lib_dir = os.environ["LIBDIR"]
+
+if "SOL_INCLUDEDIR" in os.environ:
+	include_dir = os.environ["SOL_INCLUDEDIR"]
+
+bindings_dir = os.path.join(src_root, "bindings", "python")
+
+srcs = ["mainloop.pyx", "__init__.py"]
+
+soletta_srcs = [os.path.join(bindings_dir, "soletta", src) for src in srcs]
 
 extensions = [
-    Extension("soletta", ["{}bindings/python/mainloop.pyx".format(src_root)],
-        include_dirs = ["{}build/soletta_sysroot/{}/include/soletta".format(src_root, install_prefix)],
+    Extension("soletta", soletta_srcs,
+        include_dirs = ['.', include_dir],
         libraries = ["soletta"],
-        library_dirs = ["{}build/soletta_sysroot/{}/".format(src_root, lib_prefix)])
+        library_dirs = [lib_dir])
 ]
 
+packages = ["bindings/python/soletta"]
+
 setup(
-    name = "Python Soletta",
+    name = "soletta",
     ext_modules = cythonize(extensions)
 )
